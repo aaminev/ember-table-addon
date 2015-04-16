39fbacd430eb9b91ec975257745594e24879f49a
Louis de Valliere <louis.de.valliere@gmail.com>
2015-04-16 11:12:53 -0700

    fix missing name refactor

--- app/views/scroll-panel.js
+++ app/views/scroll-panel.js
@@ -4,7 +4,7 @@ import RegisterTableComponentMixin from 'ember-table/mixins/register-table-compo
 
 export default Ember.View.extend(
 StyleBindingsMixin, RegisterTableComponentMixin, {
-  classNames: ['ember-table-scroll-panel'],
+  classNames: ['et-scroll-panel'],
   styleBindings: ['width', 'height'],
   width: Ember.computed.alias('tableComponent._tableColumnsWidth'),
   height: Ember.computed.alias('tableComponent._tableContentHeight')
