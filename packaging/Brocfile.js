a43a491b3ae7cc6862a690e6571697d8a0472673
Louis de Valliere <louis@zenefits.com>
2015-07-19 13:50:46 -0700

    wip

--- packaging/Brocfile.js
+++ packaging/Brocfile.js
@@ -1,37 +1,49 @@
 /* jshint node: true */
 
 var mergeTrees = require('broccoli-merge-trees');
-var Funnel = require('broccoli-funnel');
-// TODO(azirbel): This is deprecated
-var pickFiles = require('broccoli-funnel');
+var funnel = require('broccoli-funnel');
 // TODO(azirbel): Deprecated, remove and use es6modules
 var compileES6 = require('broccoli-es6-concatenator');
 var es3Safe = require('broccoli-es3-safe-recast');
 var templateCompiler = require('broccoli-ember-hbs-template-compiler');
-var scss = require('ember-cli-sass');
+var compileSass = require('broccoli-sass');
+
 var wrap = require('./wrap');
 var globals = require('./globals');
 
-var addonTree = pickFiles('addon', {
+var addonTree = funnel('addon', {
   srcDir: '/',
   destDir: 'ember-table'
 });
 
 // Compile templates
-var templateTree = templateCompiler('app/templates', { module: true });
-templateTree = pickFiles(templateTree, {srcDir: '/', destDir: 'ember-table/templates'});
+var templateTree = templateCompiler('app/templates', {
+  module: true
+});
+templateTree = funnel(templateTree, {
+  srcDir: '/',
+  destDir: 'ember-table/templates'
+});
 
-var sourceTree = mergeTrees([templateTree, addonTree], {overwrite: true});
+var sourceTree = mergeTrees([templateTree, addonTree], {
+  overwrite: true
+});
 
 // Does a few things:
 //   - Generate global exports, like Ember.Table.EmberTableComponent
 //   - Register all templates on Ember.TEMPLATES
 //   - Register views and components with the container so they can be looked up
 // Output goes into globals-output.js
-var globalExports = globals(pickFiles(sourceTree, {srcDir: '/ember-table', destDir: '/'}));
+var globalExports = globals(funnel(sourceTree, {
+  srcDir: '/ember-table',
+  destDir: '/'
+}));
 
 // Require.js module loader
-var loader = pickFiles('bower_components', {srcDir: '/loader.js', destDir: '/'});
+var loader = funnel('bower_components', {
+  srcDir: '/loader.js',
+  destDir: '/'
+});
 
 var jsTree = mergeTrees([sourceTree, globalExports, loader]);
 
@@ -49,17 +61,12 @@ var compiled = compileES6(jsTree, {
 compiled = wrap(compiled);
 
 // Compile scss
-var scssTree = pickFiles('addon/styles', { srcDir: '/', destDir: '/' });
+var scssTree = funnel('addon/styles', {
+  srcDir: '/',
+  destDir: '/'
+});
 var scssMain = 'addon.scss';
-var scssOutput = 'ember-table.css';
-console.log('================================================================');
-console.log(scssTree)
-console.log('=========')
-console.log(scssMain)
-console.log('=========')
-console.log(scssOutput)
-console.log('=========')
-debugger
-scssTree = compileSass(scssTree, scssMain, scssOutput);
+var scssOutputFile = 'ember-table.css';
+var scssOutput = compileSass(scssTree, scssMain, scssOutputFile);
 
-module.exports = mergeTrees([es3Safe(compiled), scssTree]);
+module.exports = mergeTrees([es3Safe(compiled), scssOutput]);
